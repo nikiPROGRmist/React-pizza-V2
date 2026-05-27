8d5fd0f881c0442e229850995600361550192f21
Кречун Никита <nkrechun@zavodnv.ru>
2026-05-27 16:39:00 +0300

    Первый коммит

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './scss/app.scss';
+import Header from './components/Header.jsx'
+import Categories from './components/Categories.jsx';
+import Sort from './components/Sort.jsx';
+import PizzaBlock from './components/PizzaBlock.jsx';
+import pizzas from './assets/pizza.json'
+
+function App() {
+  return (
+    <div classNameName="App">
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              <div className="content__items">
+                {
+                  pizzas.map((item, i) => (
+                    <PizzaBlock
+                      title={item.title}
+                      image={item.image}
+                      price={item.price} />
+
+                  ))
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Categories.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+function Categories() {
+    const [categoriesIndex, setCategoriesIndex] = useState(0)
+
+    const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
+    return (
+        <div className="categories">
+            <ul>
+                {
+                    categories.map((category, index) => (
+                        <li
+                            onClick={() => setCategoriesIndex(index)}
+                            className={categoriesIndex === index ? 'active' : ''}>
+                            {category}
+                        </li>
+
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default Categories
