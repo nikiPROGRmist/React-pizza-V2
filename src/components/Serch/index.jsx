08c79e9fe445448b201df809ba698a499d892b7d
nikiPROGRmist <krechun_2017@mail.ru>
2026-06-04 23:12:35 +0300

    Коммит 10

--- src/components/Serch/index.jsx
+++ src/components/Serch/index.jsx
@@ -3,34 +3,56 @@ import closeIMG from "../../assets/img/close.svg"
 import Styles from "./Serch.module.scss"
 import { useDispatch, useSelector } from "react-redux"
 import { setSerch } from "../../redux/slices/filterSlice"
-import { useEffect } from "react"
-
-
-
+import { useEffect, useRef, useState } from "react"
 
 
 export const Serch = () => {
     const serch = useSelector(state => state.filter.serch)
+    const dispatch = useDispatch()
+    const refFocus = useRef()
+    const [inputValue, setInputValue] = useState(serch)
+    const timer = useRef(null)  // просто timer вместо timeoutRef
 
     useEffect(() => {
-        document.querySelector("input").focus()
-    }, [serch])
+        refFocus.current.focus()
+    }, [])
 
-    const dispatch = useDispatch()
+    const handleChange = (value) => {
+        setInputValue(value)
+
+        if (timer.current) clearTimeout(timer.current)
+
+        timer.current = setTimeout(() => {
+            dispatch(setSerch(value))
+        }, 1500)
+    }
+
+    const handleClear = () => {
+        setInputValue("")
+        dispatch(setSerch(""))
+    }
+
     return (
         <div className={Styles.root}>
             <div className={Styles.root_input}>
-                <img className={Styles.root_img} src={serchLogo} alt="SerchLogo" />
-                <input className={Styles.input_img}
-                    value={serch}
-                    onChange={event => dispatch(setSerch(event.target.value))}
-                    type="serch" placeholder="Поиск" />
-                {serch.length > 0 ?
-                    <img onClick={() => dispatch(setSerch(""))}
-                        className={Styles.root_close}
-                        src={closeIMG} alt="close" />
-                    : false}
+                <div className={Styles.input__item}>
+                    <img className={Styles.root_img} src={serchLogo} alt="SerchLogo" />
+                    <input
+                        ref={refFocus}
+                        className={Styles.input_img}
+                        value={inputValue}
+                        onChange={e => handleChange(e.target.value)}
+                        type="text"
+                        placeholder="Поиск"
+                    />
+                    <img
+                        onClick={handleClear}
+                        className={inputValue ? Styles.root_active : Styles.root_close}
+                        src={closeIMG}
+                        alt="close"
+                    />
+                </div>
             </div>
         </div>
     )
-}
+} 
