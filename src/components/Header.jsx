8f0f5acbced20da057ea527e395a6869ab1b713e
nikiPROGRmist <krecun_2017@mail.ru>
2026-06-23 00:58:26 +0300

    коммит 17

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router"
 import logoSVG from "../assets/img/pizza-logo.svg"
 import { Serch } from "./Serch"
+import { useSelector } from "react-redux"
 function Header() {
+    const { items, totalPrice } = useSelector(state => state.cart)
+
+
     return (
         <div className="header">
             <div className="container">
@@ -19,7 +23,7 @@ function Header() {
                 <div className="header__cart">
                     <Link to="/cart">
                         <a href="/cart.html" className="button button--cart">
-                            <span>520 ₽</span>
+                            <span>{totalPrice}</span>
                             <div className="button__delimiter"></div>
                             <svg
                                 width="18"
@@ -50,7 +54,7 @@ function Header() {
                                     strokeLinejoin="round"
                                 />
                             </svg>
-                            <span>3</span>
+                            <span>{items.length}</span>
                         </a>
                     </Link>
                 </div>
--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     items: [],
+    totalPrice: 0,
 
 }
 
@@ -12,18 +13,63 @@ export const cartSlice = createSlice({
 
     reducers: {
         setAddItems(state, action) {
-            state.items.push(action.payload)
+
+            const existing = state.items.find(item => item.id === action.payload.id)
+
+            if (!existing) {
+                state.items.push(action.payload)
+
+            } else {
+                existing.count += 1;
+            }
+
+            state.totalPrice = state.items.reduce((sum, item) => sum + item.price * item.count, 0)
+
         },
 
+
         setRemoveItems(state, action) {
-            state.items.filter(obj => obj.id === action.payload.id)
-            console.log(action.payload.id)
+            state.items = state.items.filter(obj => obj.id !== action.payload)
+            state.totalPrice = state.items.reduce((sum, item) => sum + item.price * item.count, 0);
+        },
+
+        setCountPlus(state, action) {
+            const item = state.items.find(obj => obj.id === action.payload);
+            if (item) {
+                item.count += 1;
+                state.totalPrice = state.items.reduce(
+                    (sum, item) => sum + item.price * item.count,
+                    0
+                );
+            }
+        },
+
+        setCountMinus(state, action) {
+            const item = state.items.find(obj => obj.id === action.payload);
+
+            if (item) {
+                if (item.count > 1) {
+                    item.count -= 1;
+                }
+
+                state.totalPrice = state.items.reduce(
+                    (sum, item) => sum + item.price * item.count,
+                    0
+                );
+            }
+        },
+
+
+
+        setClearItems(state, action) {
+            state.items = action.payload
+            state.totalPrice = 0
         }
 
 
     }
 })
 
-export const { setAddItems, setRemoveItems } = cartSlice.actions
+export const { setAddItems, setRemoveItems, setClearItems, setCount, setCountPlus, setCountMinus } = cartSlice.actions
 
 export default cartSlice.reducer
